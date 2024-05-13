65939093e1a54d15406a75515f34db458c00a500
Pablo Lara Roloff <pablo.lara.roloff@gmail.com>
2024-05-13 13:17:30 +0200

    app not rendering

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -6,15 +6,16 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-user-registration-form',
   templateUrl: './user-registration-form.component.html',
-  styleUrl: './user-registration-form.component.scss',
+  styleUrl: './user-registration-form.component.scss', 
 })
 export class UserRegistrationFormComponent implements OnInit {
   @Input() userData = {
     UserName: '',
     Password: '',
     Email: '',
-    Birthdate: Date,
+    Birthdate: '', 
   };
+
   constructor(
     public fetchApiData: FetchApiDataService,
     public dialogRef: MatDialogRef<UserRegistrationFormComponent>,
@@ -22,18 +23,21 @@ export class UserRegistrationFormComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {}
-  // This is the function responsible for sending the form inputs to the backend
+
   registerUser(): void {
     this.fetchApiData.userRegistration(this.userData).subscribe(
-      (result) => {
-        // Logic for a successful user registration goes here! (To be implemented)
-        this.dialogRef.close(); // This will close the modal on success!
-        this.snackBar.open(result, 'OK', {
+      (result: any) => {
+        this.dialogRef.close();
+        this.snackBar.open(result.message, 'OK', {
           duration: 2000,
         });
       },
-      (result) => {
-        this.snackBar.open(result, 'OK', {
+      (error: any) => {
+        let errorMessage = 'An error occurred. Please try again.';
+        if (error.error && error.error.message) {
+          errorMessage = error.error.message;
+        }
+        this.snackBar.open(errorMessage, 'OK', {
           duration: 2000,
         });
       }
