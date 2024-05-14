2c81bf7c995c69beb300fb1eace6af32e13ab57f
Pablo Lara Roloff <pablo.lara.roloff@gmail.com>
2024-05-14 13:43:35 +0200

    working Angular app

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { FetchApiDataService } from '../fetch-api-data.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -8,24 +8,26 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   templateUrl: './user-registration-form.component.html',
   styleUrls: ['./user-registration-form.component.scss']
 })
-
 export class UserRegistrationFormComponent implements OnInit {
   @Input() userData = { Username: '', Password: '', Email: '', Birthday: '' };
 
   constructor(
     public fetchApiData: FetchApiDataService,
     public dialogRef: MatDialogRef<UserRegistrationFormComponent>,
-    public snackBar: MatSnackBar
-  ) {}
+    public snackBar: MatSnackBar) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 
   registerUser(): void {
     this.fetchApiData.userRegistration(this.userData).subscribe((result) => {
       this.dialogRef.close();
-      this.snackBar.open(result, 'OK', { duration: 2000 });
+      this.snackBar.open(result, 'OK', {
+        duration: 2000
+      });
     }, (result) => {
-      this.snackBar.open(result, 'OK', { duration: 2000 });
+      this.snackBar.open(result, 'OK', {
+        duration: 2000
+      });
     });
   }
 }
